27a71e7990a8b6769a277b6dbcdbb2eb7ea591ad
boryc <lborycki@gmail.com>
2024-04-21 23:27:38 +0200

    fixes

--- footswitchAmpero/footswitchAmpero.ino
+++ footswitchAmpero/footswitchAmpero.ino
@@ -19,18 +19,18 @@ int record = 1;
 int play = 2;
 int stop = 3;
 
-int status = stop;
+int status = play;
 
 
 
 // the follow variables are long's because the time, measured in miliseconds,
 // will quickly become a bigger number than can be stored in an int.
 long time = 0;         // the last time the output pin was toggled
-long debounce = 100;   // the debounce time, increase if the output flickers
+long debounce = 100; // the debounce time, increase if the output flickers
+long clear = 2000;  // clear time
 
 void setup()
 {
-//  Serial.begin(9600); // setup serial for MIDI
   Serial.begin(31250); // setup serial for MIDI
 
   Serial.println("start");
@@ -53,15 +53,22 @@ void loop()
   // the time
   if (reading1 != previous1 && millis() - time > debounce) {
 
-    if(status == stop || status == play) {
-      Serial.println("Send Record/Overdub");
+    if(status == stop ) {
+      Serial.println("Send Play, Status Play");
+      midiOut.sendControlChange(64,127,1); 
+     
+      status = play;
+      
+    }
+    else if(status == play) {
+      Serial.println("Send Record/Overdub, Status Record");
       midiOut.sendControlChange(63,0,1); 
      
       status = record;
       
     }
     else {
-      Serial.println("Send Play");
+      Serial.println("Send Play, Status Play");
       midiOut.sendControlChange(64,127,1); 
       status = play;
     }
@@ -72,11 +79,12 @@ void loop()
  
   if (reading2 != previous2 && millis() - time > debounce) {
          
-    Serial.println("Send Stop");
+    Serial.println("Send Stop, Status Stop");
     midiOut.sendControlChange(64,0,1); 
 
     status = stop;
     time = millis();
     previous2 = reading2;    
   }
+  
 }
